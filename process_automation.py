8f44b0c880f7433b4189493bc7195b11956283cd
matias_estigarribia <matiestiga@gmail.com>
2024-02-13 09:30:18 -0300

    Version 1.0 POO with log

--- process_automation.py
+++ process_automation.py
@@ -1,6 +1,6 @@
 import time
 import pyautogui
-
+import logging
 
 
 class ProcessAutomation():
@@ -8,6 +8,12 @@ class ProcessAutomation():
     def  __init__(self):
         pyautogui.FAILSAFE=True
         self.message = "Process automation with Python and pyautogui succesfull"
+        logging.basicConfig(
+                    filename = 'my_log.log',
+                    level = logging.INFO,
+                    filemode =  'w',
+                    format = '%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s - %(message)s'
+)
     
     def open_soft(self, software):
         pyautogui.hotkey('win', 'r')
@@ -18,20 +24,23 @@ class ProcessAutomation():
         time.sleep(2)
     def write_message(self, software):
         pyautogui.write(self.message)
-        print("Message written in", software, "window.")
+        logging.info("Message written in" + software + "window.")
+        
     def save_arquive(self, software):
         pyautogui.hotkey('ctrl', 's')
         time.sleep(1)
         pyautogui.write("automatic_file_" + time.strftime("%d.%m.%Y"))  #name of the file with date
         pyautogui.press('enter')
         
-        print("File succesfully saved on " + software + " under automatic_file_" + time.strftime("%d.%m.%Y"))
+        logging.info("File succesfully saved on " + software + " under automatic_file_" + time.strftime("%d.%m.%Y"))
+        
 
         time.sleep(1)
         
         pyautogui.hotkey('alt', 'f4')   #close window
         
-        print("Window closed successfully!")
+        
+        logging.info("Window closed successfully!")
     def process_automation(self, software):
         self.open_soft(software)
         self.write_message(software)
