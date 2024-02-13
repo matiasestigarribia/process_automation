d1b577fb2ac627a95ee772a64870d5739a123daf
matias_estigarribia <matiestiga@gmail.com>
2024-02-13 09:12:59 -0300

    Version 1.0 POO

--- process_automation.py
+++ process_automation.py
@@ -1,38 +1,47 @@
 import time
 import pyautogui
 
-def process_automation(software):
-    try:
-        #open software
+
+
+class ProcessAutomation():
+    
+    def  __init__(self):
+        pyautogui.FAILSAFE=True
+        self.message = "Process automation with Python and pyautogui succesfull"
+    
+    def open_soft(self, software):
         pyautogui.hotkey('win', 'r')
         pyautogui.write(software + ".exe")
         pyautogui.press('enter')
         
         #wait time for software to open
         time.sleep(2)
-        
-        #writing on software
-        message = "Process automation with Python and pyautogui succesfull"
-        pyautogui.write(message)
+    def write_message(self, software):
+        pyautogui.write(self.message)
         print("Message written in", software, "window.")
-
-        #saving arquive
+    def save_arquive(self, software):
         pyautogui.hotkey('ctrl', 's')
         time.sleep(1)
-        pyautogui.write("automatic_file_" + time.strftime("%d%m%Y"))  #name of the file with date
+        pyautogui.write("automatic_file_" + time.strftime("%d.%m.%Y"))  #name of the file with date
         pyautogui.press('enter')
         
-        print("File succesfully saved on " + software + " under automatic_file_" + time.strftime("%d/%m/%Y"))
+        print("File succesfully saved on " + software + " under automatic_file_" + time.strftime("%d.%m.%Y"))
 
         time.sleep(1)
         
         pyautogui.hotkey('alt', 'f4')   #close window
         
         print("Window closed successfully!")
+    def process_automation(self, software):
+        self.open_soft(software)
+        self.write_message(software)
+        self.save_arquive(software)
+        ...
         
-    except:
-        
-        print("An error ocurred while trying to access ", software)
+if  __name__ == "__main__":
+    myProcess = ProcessAutomation()
+    myProcess.process_automation("Notepad")
     
-if __name__ == "__main__":
-    process_automation("Notepad")   #you can change Notepad for other text editor
+    
+ 
+
