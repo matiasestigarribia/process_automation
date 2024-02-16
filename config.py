e03559c6cb4c13b2cd46c1f528d0934915028271
matias_estigarribia <matiestiga@gmail.com>
2024-02-16 12:24:24 -0300

    Versao 1.0 final

---
+++ config.py
@@ -0,0 +1,6 @@
+SLEEP_TIMES = {
+    'open_software' : 2, 
+    'save_file' : 1,
+    'cancel_save':1, 
+    'close_window':2    
+}
--- process_automation.py
+++ process_automation.py
@@ -1,6 +1,8 @@
 import time
 import pyautogui
 import logging
+import os
+from config import SLEEP_TIMES
 
 
 class ProcessAutomation():
@@ -8,44 +10,62 @@ class ProcessAutomation():
     def  __init__(self):
         pyautogui.FAILSAFE=True
         self.message = "Process automation with Python and pyautogui succesfull"
+        self.project_directory = os.path.dirname(os.path.abspath(__file__))
         logging.basicConfig(
                     filename = 'my_log.log',
                     level = logging.INFO,
                     filemode =  'w',
                     format = '%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s - %(message)s'
-)
+                    )
     
     def open_soft(self, software):
         pyautogui.hotkey('win', 'r')
         pyautogui.write(software + ".exe")
         pyautogui.press('enter')
         
-        #wait time for software to open
-        time.sleep(2)
+        
+        time.sleep(SLEEP_TIMES['open_software'])
     def write_message(self, software):
         pyautogui.write(self.message)
-        logging.info("Message written in" + software + "window.")
+        logging.info("Message written in " + software + " window.")
+    
+    def check_and_modify_file_name(self, file_name):
+        i=1
+        while os.path.exists(os.path.join(self.project_directory, file_name)):
+            logging.warning("File name already exist")
+            file_name = file_name[:-4] + "_" + str(i) + file_name[-4:]
+            i+=1
+        return file_name
+    
+    def save_arquive(self, software, save_path=None):
+        if save_path is None:
+            save_path = os.path.join(self.project_directory)
         
-    def save_arquive(self, software):
         pyautogui.hotkey('ctrl', 's')
-        time.sleep(1)
-        pyautogui.write("automatic_file_" + time.strftime("%d.%m.%Y"))  #name of the file with date
+        
+        
+        #comprobar si o nome de arquivo ja existe
+        file_name = "automatic_file_" + time.strftime("%d.%m.%Y" + ".txt")
+        file_name = self.check_and_modify_file_name(file_name)
+        
+        
+        file_path = os.path.join(save_path, file_name)
+        pyautogui.write(file_path)  
         pyautogui.press('enter')
+        time.sleep(SLEEP_TIMES['save_file'])
         
-        logging.info("File succesfully saved on " + software + " under automatic_file_" + time.strftime("%d.%m.%Y"))
+        logging.info("File succesfully savedd on " + software + " under " + file_name)
         
-
-        time.sleep(1)
-        
+        time.sleep(SLEEP_TIMES['close_window'])
+        logging.info("Automation completed successfully!")
         pyautogui.hotkey('alt', 'f4')   #close window
-        
-        
-        logging.info("Window closed successfully!")
+     
+
     def process_automation(self, software):
         self.open_soft(software)
         self.write_message(software)
         self.save_arquive(software)
-        ...
+        
         
 if  __name__ == "__main__":
     myProcess = ProcessAutomation()
